e188568d35b9ed7976e1b5537bbd373fe4c66c45
Lukas Pereira <diagolukas483@gmail.com>
2023-07-27 16:19:26 -0300

    add images in front-end

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,3 +1,4 @@
+
 const divBebida = document.querySelector(".bebidas");
 const spanBadge = document.querySelector("span.position-absolute");
 
@@ -9,41 +10,50 @@ const btnLimpar = document.querySelector("#btLimpar")
 
 const tabBebidas = document.querySelector("#tabBebida");
 
+
 let bebidasCompradas;
 let bebidas;
 
 const carregaBebidas = async () => {
-  const dados = await axios.get("./databases/ratao.db");
+  try {
+    const response = await axios.get('http://localhost:3000/bebidas');
+    const bebidas = response.data;
 
-  bebidas = dados.data;
+    let resposta = '';
 
-  let resposta = "";
+    for (const bebida of bebidas) {
+      // Codificar a imagem em base64
+      const fotoBase64 = bebida.foto.toString('base64');
 
-  for (const bebida of bebidas) {
-    resposta += `
-    <div class="col-6 col-sm-4 col-md-3">
-      <div class="card">
-      <img src="${bebida.foto}" class="card-img-top" alt="tragos">
-        <div class="card-body">
-          <h5 class="card-title">${bebida.id} - ${bebida.marca}</h5>
-          <p class="card-text">${bebida.tipo}</p>
-          <p class="card-text">R$: 
-${bebida.preco.toLocaleString("pt-br", { minimumFractionDigits: 2 })}</p>
-          <button class="btn btn-primary btAdicionar">Adicionar</button>
-        </div>
-      </div>    
-    </div>  
-    `;
+      resposta += `
+        <div class="col-6 col-sm-4 col-md-3">
+          <div class="card">
+            <img src="data:images/jpeg;base64,${fotoBase64}" class="card-img-top" alt="tragos">
+            <div class="card-body">
+              <h5 class="card-title">${bebida.estoque} - ${bebida.tipo}</h5>
+              <p class="card-text">${bebida.nome}</p>
+              <p class="card-text">R$: 
+                ${bebida.preco.toLocaleString('pt-br', { minimumFractionDigits: 2 })}
+              </p>
+              <button class="btn btn-primary btAdicionar">Adicionar</button>
+            </div>
+          </div>    
+        </div>  
+      `;
+    }
+    divBebida.innerHTML = resposta;
+
+    bebidasCompradas = localStorage.getItem('bebidas')
+      ? localStorage.getItem('bebidas').split(';')
+      : [];
+
+    spanBadge.innerText = bebidasCompradas.length;
+  } catch (error) {
+    console.error('Erro ao carregar bebidas:', error);
   }
-
-  divBebida.innerHTML = resposta;
-
-  bebidasCompradas = localStorage.getItem("./databases/ratao.db")
-    ? localStorage.getItem("bebidas").split(";")
-    : [];
-
-  spanBadge.innerText = bebidasCompradas.length;
 };
+
+carregaBebidas()
 
 window.addEventListener("load", carregaBebidas);
 
@@ -85,8 +95,8 @@ btnCarrinho.addEventListener("click", () => {
     const col2 = linha.insertCell(1);
     const col3 = linha.insertCell(2);
 
-    col1.innerHTML = `<img src="${bebida.foto}" alt="${bebida.marca}" width="80">`;
-    col2.innerText = bebida.marca;
+    col1.innerHTML = `<img src="${bebida.foto}" alt="${bebida.nome}" width="80">`;
+    col2.innerText = bebida.tipo;
     col3.innerText = bebida.preco.toLocaleString("pt-br", {
       minimumFractionDigits: 2,
     });
@@ -111,7 +121,7 @@ btnCarrinho.addEventListener("click", () => {
 btnFinalizar.addEventListener("click", () => {
   myModal.hide();
 
-  localStorage.removeItem("./databases/ratao.db");
+  localStorage.removeItem("bebidas");
 
   alert("Valeu fera! Sua Bebida chegará em 5 min");
 
@@ -120,7 +130,7 @@ btnFinalizar.addEventListener("click", () => {
 
 btnLimpar.addEventListener("click", () => {
 
-  localStorage.removeItem("./databases/ratao.db");
+  localStorage.removeItem("bebidas");
  
   for (let i = tabBebidas.rows.length - 1; i >= 1; i--) {
     tabBebidas.deleteRow(i);
